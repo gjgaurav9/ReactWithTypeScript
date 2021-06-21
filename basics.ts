09d7e7f975140da2a9b643649b531857cad73764
Gaurav Jain <gjgaurav9@gmail.com>
2021-06-21 17:49:31 +0530

    Learnt basic types

---
+++ basics.ts
@@ -0,0 +1,45 @@
+// primitive types : number , string , boolean
+// complex types : arrays, object
+// Function type : parameters
+
+// primitive types
+
+let myNum: number;
+
+myNum = 10.3;
+
+let myString: string;
+
+myString = "hello";
+
+let myBool: boolean;
+
+myBool = false;
+
+// Complex Types
+
+let myStrArr: string[];
+
+myStrArr = ["name", "hell"];
+
+let myPersonObj: {
+  name: string;
+  age: number;
+};
+
+myPersonObj = {
+  name: "Gaurav",
+  age: 4,
+};
+
+let myObjectArr: {
+  name: string;
+  age: number;
+}[];
+
+myObjectArr = [
+  {
+    name: "Gaurav",
+    age: 4,
+  },
+];
