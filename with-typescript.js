17050a81d11bd7ed50d331d504fa381de5c41398
Gaurav Jain <gjgaurav9@gmail.com>
2021-06-21 17:50:01 +0530

    Introduced typescript in the project

---
+++ with-typescript.js
@@ -0,0 +1,5 @@
+function addTs(a, b) {
+    return a + b;
+}
+var result = addTs(2, 5);
+console.log(result);
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+function addTs(a: number, b: number) {
+  return a + b;
+}
+
+const result = addTs(2, 5);
+
+console.log(result);
